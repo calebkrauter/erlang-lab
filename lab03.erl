db5be433c36d5fce53ecb925708343417f7904aa
Terence F Trajano <ttrajano@uw.edu>
2024-04-09 10:30:32 -0700

    lab 3 foundation

---
+++ lab03.erl
@@ -0,0 +1,39 @@
+% Caleb Krauter
+% Terence Fornelosa Trajano
+% Jonathan Kim
+
+-module(lab03).
+%-export([]).
+-compile(export_all).
+
+%-import(io,[format/2]).
+
+% Problem 1.
+
+
+% Problem 2.
+
+
+% Problem 3.
+
+
+% Problem 4
+
+
+% Problem 5
+
+
+% Problem 6
+
+
+% Problem 7
+
+
+% Problem 8
+
+
+% Problem 9
+
+
+% Problem 10
+
