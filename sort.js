fc1aedfd09d755e0ad7da86c3e0e3c7cc734d5bf
Caleb Krauter <ghostMe66+github@proton.me>
2024-05-01 19:23:07 -0700

    Complete lab.

--- sort.js
+++ sort.js
@@ -1,24 +1,33 @@
-newArrayThatIsSorted = [];
 function mergeSort(arr, comp) {
     if (arr.length <= 1) {
         return arr;
     }
-    mid = arr.length / 2
-    left = mergeSort(arr.slice(0, mid), comp)
-    right = mergeSort(arr.slice(mid + 1, arr.length - 1), comp)
-    newArrayThatIsSorted = [...newArrayThatIsSorted, merge(left, right, comp)]
-    return newArrayThatIsSorted;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid), comp);
+    let right = mergeSort(arr.slice(mid, arr.length), comp);
+
+    return merge(left, right, comp);
 }
 
-const comp = (A, B) => A <= B;
+const comp = (A, B) => A >= B;
 
 function merge(left, right, comp) {
+    let sortedArray = [];
     while (left.length > 0 && right.length > 0) {
-        if (comp(left[0][], right[0])) {
-
+        if (comp(left[0], right[0])) {
+            sortedArray.push(left.shift());
+        } else {
+            sortedArray.push(right.shift());
         }
     }
-    return comp(left, right)
+
+    if (left.length == 0) {
+        sortedArray = sortedArray.concat(right);
+    }
+    if (right.length == 0) {
+        sortedArray = sortedArray.concat(left);
+    }
+    return sortedArray;
 }
 
-console.log(mergeSort([9, 8, 7, 6, 5, 4, 3, 2, 1], comp))
+//console.log(mergeSort([-1, 999, 1, 2, 3, 2, 3, 12, 3, 4, 6, 9, 3, 2, 1], (A, B) => { return A >= B; }));
